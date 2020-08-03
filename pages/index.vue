7e387f9f4d90c4cefa7e10dab88e2b79f2c103a3
Johan Ronsse <johan.ronsse@gmail.com>
2020-08-03 10:51:37 +0200

    Improve markup of sign in screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <main>
-      <h1 class="c-h1">App name!</h1>
+      <h1 class="c-h1">App name</h1>
       <div class="c-form-group">
-        <Label>E-mail</Label>
-        <Input type="text" placeholder="naam@email.com"/>
+        <label for="email">E-mail</label>
+        <input type="text" id="email" class="c-input" placeholder="naam@email.com" />
       </div>
       <div class="c-form-group">
-        <Label>Wachtwoord</Label>
-        <Input type="password" placeholder="Jouw wachtwoord"/>
+        <label for="password">Password</label>
+        <input type="password" id="password" class="c-input" placeholder="Your password" />
       </div>
-      <a href="/app/dashboard" class="c-button c-button--primary">Sign in...</a>
+      <a href="/app/dashboard" class="c-button c-button--primary">Sign in</a>
     </main>
   </div>
 </template>
